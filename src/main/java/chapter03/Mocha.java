706f7d5af676a67bcc3d518569c5e5a763903744
shinaaaa <shin029abc@gmail.com>
2023-05-18 01:56:11 +0900

    feat: 데코레이터 패턴 정리

---
+++ src/main/java/chapter03/Mocha.java
@@ -0,0 +1,18 @@
+package chapter03;
+
+public class Mocha extends CondimentDecorator {
+
+    public Mocha(Beverage beverage) {
+        this.beverage = beverage;
+    }
+
+    @Override
+    public double cost() {
+        return beverage.cost() + 0.20;
+    }
+
+    @Override
+    public String getDescription() {
+        return beverage.getDescription() + ", 모카";
+    }
+}
