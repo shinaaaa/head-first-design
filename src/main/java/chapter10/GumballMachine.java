b2f535498e16eaa45b73f9ebc1c6bf7b3b3c7b6c
shinaaaa <shin029abc@gmail.com>
2023-07-15 21:24:58 +0900

    feat: 상태 패턴 추가

---
+++ src/main/java/chapter10/GumballMachine.java
@@ -0,0 +1,80 @@
+package chapter10;
+
+public class GumballMachine {
+
+    private final State soldOutState;
+    private final State noQuarterState;
+    private final State hasQuarterState;
+    private final State soldState;
+    State state;
+    int count = 0;
+
+    public GumballMachine(int numberGumballs) {
+        soldOutState = new SoldOutState(this);
+        noQuarterState = new NoQuarterState(this);
+        hasQuarterState = new HasQuarterState(this);
+        soldState = new SoldState(this);
+
+        this.count = numberGumballs;
+        if (numberGumballs > 0) {
+            state = noQuarterState;
+        } else {
+            state = soldOutState;
+        }
+    }
+
+    public void insertQuarter() {
+        state.insertQuarter();
+    }
+
+    public void ejectQuarter() {
+        state.ejectQuarter();
+    }
+
+    public void turnCrank() {
+        state.turnCrank();
+    }
+
+    public void setState(State state) {
+        this.state = state;
+    }
+
+    void releaseBall() {
+        System.out.println("알맹이를 내보내고 있습니다.");
+        if (count > 0) {
+            count--;
+        }
+    }
+
+    public State getSoldOutState() {
+        return soldOutState;
+    }
+
+    public State getNoQuarterState() {
+        return noQuarterState;
+    }
+
+    public State getHasQuarterState() {
+        return hasQuarterState;
+    }
+
+    public State getSoldState() {
+        return soldState;
+    }
+
+    public State getState() {
+        return state;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    @Override
+    public String toString() {
+        return "GumballMachine{" +
+                "state=" + state +
+                ", count=" + count +
+                '}';
+    }
+}
---
+++ src/main/java/chapter10/State.java
@@ -0,0 +1,11 @@
+package chapter10;
+
+public interface State {
+    void insertQuarter();
+
+    void ejectQuarter();
+
+    void turnCrank();
+
+    void dispense();
+}
